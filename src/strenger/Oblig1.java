d9105fc493b652efd5b02f6c18af2c2d810de457
BaharKrc <144059055+BaharKrc@users.noreply.github.com>
2024-09-04 16:58:43 +0200

    oppgaver 5

---
+++ src/strenger/Oblig1.java
@@ -0,0 +1,8 @@
+package strenger;
+
+public class Oblig1 {
+    public static void main(String[]args){
+        String navn = "Bahar";
+        System.out.println("Tenk at jeg, "+navn+", har naila oblig 1 allerede!");
+    }
+}
